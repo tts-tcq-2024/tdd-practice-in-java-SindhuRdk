026290b164e122e4c37549188925510c16e8cf6a
SindhuRdk <52288683+SindhuRdk@users.noreply.github.com>
2024-09-20 10:04:08 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -1,2 +1,11 @@
 package TddPracticeInJava;
 
+public class StringCalculator {
+
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        return -1;  // Default return to fail for now
+    }
+}
